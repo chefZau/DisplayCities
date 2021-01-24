9535ac058942bd1905d1662980ea7ff9d0e42c56
Luca(MingCong) Zhou <zhoumingcong6366@gmail.com>
2021-01-23 23:35:37 -0500

    create serial id for class

--- CityMarker.java
+++ CityMarker.java
@@ -2,6 +2,11 @@ import javax.swing.ImageIcon;
 
 public class CityMarker extends ImageIcon {
 	
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 2785676011617124091L;
+
 	public CityMarker() {
 		super("marker_city.png");
 	}
