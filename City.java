5ce8630256c94705787f32a7c7e3c0102a36e2e7
Luca(MingCong) Zhou <zhoumingcong6366@gmail.com>
2021-01-24 00:17:29 -0500

    create setter and getter methods for all attributes

--- City.java
+++ City.java
@@ -11,11 +11,46 @@ public class City {
     private Integer y;
     private CityMarker marker;
 
-    public City(String name, int x, int y) {
-        this.name = name;
-        this.x = x;
-        this.y = y;
+    public City(String newName, int newX, int newY) {
+        this.name = newName;
+        this.x = newX;
+        this.y = newY;
         this.marker = new CityMarker();
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public CityMarker getMarker() {
+        return this.marker;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void setX(Integer newX) {
+        this.x = newX;
+    }
+
+    public void setY(Integer newY) {
+        this.y = newY;
+    }
+
+    public void setMarker(CityMarker newMarker) {
+        this.marker = newMarker;
+    }
+
+    public String toString() {
+        return this.name;
+    }
 }
