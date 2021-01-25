20b5764f9f33df2e2e30e5d1c2a1234fa681b7d1
Luca(MingCong) Zhou <zhoumingcong6366@gmail.com>
2021-01-24 21:57:12 -0500

    change path for the resource image

--- luca_asn1/CityMarker.java
+++ luca_asn1/CityMarker.java
@@ -4,13 +4,10 @@ import javax.swing.ImageIcon;
 
 public class CityMarker extends ImageIcon {
 
-	/**
-	 *
-	 */
 	private static final long serialVersionUID = 2785676011617124091L;
 
 	public CityMarker() {
-		super("marker_city.png");
+		super("./resource/marker_city.png");
 	}
 
 }
