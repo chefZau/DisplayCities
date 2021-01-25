f786ab8ccaa205a2075328ad523836e29d2ffa23
Luca(MingCong) Zhou <zhoumingcong6366@gmail.com>
2021-01-24 19:18:48 -0500

    move all source files to folder src

---
+++ src/Program.java
@@ -0,0 +1,65 @@
+/**
+ * The Program class, as its name suggests, will be the main heart of the
+ * program. It will be the entry point of the program, read in a file of cities
+ * and create objects for each of them, contain the array of those cities, and
+ * create a CompressedArray containing the distances between each of the cities
+ * read in from the file.
+ * 
+ * Created by Luca Zhou on 1/25/2021.
+ */
+public class Program {
+
+    private Integer cityCount;
+    private City[] cityArray;
+    private CompressedArray array;
+
+    /**
+     * 
+     * @param file
+     * @param showMap
+     */
+    public Program(String file, boolean showMap) {
+        
+    }
+
+    /**
+     * Return attribute cityArray
+     * 
+     * @return attribute cityArray
+     */
+    public City[] getCityList() {
+
+    }
+
+    /**
+     * Expands the capacity of cityArray by adding 3 additional slots to the array
+     */
+    private void expandCapacity() {
+        
+    }
+
+    /**
+     * Calculates the Euclidean distance between the two given Cities
+     * 
+     * @return the Euclidean distance
+     */
+    public double distBetweenCities(City city1, City city2) {
+
+    }
+
+    /**
+     * 
+     */
+    public void compareDistances() {
+
+    }
+
+    /**
+     * Returns the CompressedArray array object.
+     * 
+     * @return attribute array
+     */
+    public CompressedArray getArray() {
+
+    }
+}
--- src/TestCompressedArray.java
+++ src/TestCompressedArray.java
@@ -1,5 +1,20 @@
-
 public class TestCompressedArray {
+	public static String unEscapeString(String s) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s.length(); i++)
+			switch (s.charAt(i)) {
+				case '\n':
+					sb.append("\\n");
+					break;
+				case '\t':
+					sb.append("\\t");
+					break;
+				// ... rest of escape characters
+				default:
+					sb.append(s.charAt(i));
+			}
+		return sb.toString();
+	}
 
 	public static void main(String[] args) {
 
