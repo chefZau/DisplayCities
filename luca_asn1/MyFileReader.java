622277c45c5a4babc10f830cd5626f2c6939739b
Luca(MingCong) Zhou <zhoumingcong6366@gmail.com>
2021-01-24 19:47:12 -0500

    format the file

--- luca_asn1/MyFileReader.java
+++ luca_asn1/MyFileReader.java
@@ -9,26 +9,29 @@ import java.io.IOException;
 public class MyFileReader {
 	private String buffer = null;
 	private BufferedReader in = null;
-	
+
 	/* Open the file whose name was give as parameter */
 	public MyFileReader(String fileName) {
 		try {
 			in = new BufferedReader(new FileReader(fileName));
-			buffer = in.readLine();  // Store first line
-		}
-		catch (IOException e) {
-			System.out.println("Cannot read file \'"+fileName+"\'");
+			buffer = in.readLine(); // Store first line
+		} catch (IOException e) {
+			System.out.println("Cannot read file \'" + fileName + "\'");
 			System.exit(0);
 		}
 	}
-	
-	/* Returns true if the end of the file has been reached; 
-	   it returns false otherwise.                           */
+
+	/*
+	 * Returns true if the end of the file has been reached; it returns false
+	 * otherwise.
+	 */
 	public boolean endOfFile() {
-		if (buffer == null) return true;
-		else return false;
+		if (buffer == null)
+			return true;
+		else
+			return false;
 	}
-	
+
 	/* Reads a line from the file. */
 	public String readString() {
 		String line = buffer;
@@ -38,25 +41,28 @@ public class MyFileReader {
 		}
 		try {
 			buffer = in.readLine();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("Error. Cannot read from file");
 			System.exit(0);
 		}
 		return line;
 	}
-	
+
 	/* Reads an integer from the file. Returns -1 if no integer could be read. */
 	public int readInt() {
 		String line = readString();
-		if (line == null) return -1;
-		else return Integer.parseInt(line);
+		if (line == null)
+			return -1;
+		else
+			return Integer.parseInt(line);
 	}
-	
+
 	/* Reads an double from the file. Returns -1.0 if no integer could be read. */
 	public double readDouble() {
 		String line = readString();
-		if (line == null) return -1.0;
-		else return Double.parseDouble(line);
+		if (line == null)
+			return -1.0;
+		else
+			return Double.parseDouble(line);
 	}
 }
